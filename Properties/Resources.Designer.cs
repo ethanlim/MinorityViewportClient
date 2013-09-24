266d80f8cfbaa68314d5dd67506a36f674501ba8
ethanlim <mail@ethanlim.net>
2013-09-25 00:03:53 +0800

    Change project name

--- Properties/Resources.Designer.cs
+++ Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace MultipleKinectsPlatform.Properties {
+namespace MultipleKinectsPlatformClient.Properties {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace MultipleKinectsPlatform.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("MultipleKinectsPlatform.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("MultipleKinectsPlatformClient.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
